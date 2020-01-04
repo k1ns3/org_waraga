10b4b4f8d9b9858d3c5d847a1d68fda529701f50
k1ns3 <43415209+k1ns3@users.noreply.github.com>
2020-01-04 17:27:22 +0300

    initial

---
+++ src/app/organizer/validators/lat-string.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function stringValidator(pattern?: object): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+        const currentPattern = pattern || /[a-zA-Z]/g;
+        const str = control.value;
+
+        return str.match(currentPattern) ? { isInvalidString: true } : null;
+    }
+} 
