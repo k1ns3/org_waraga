a73774da45dff8db2b9b0a46727816d2d69992cc
Кондратьев Павел Дмитриевич <p.kondratiev@scout-gps.ru>
2020-02-19 13:05:39 +0300

    [master][add] State Model

--- src/app/organaizer.state.ts
+++ src/app/organaizer.state.ts
@@ -4,6 +4,22 @@ import {
     SelectedMonth
 } from './organaizer.actions'
 
+interface OrganaizerStateModel { 
+    Day: string;
+    Month: string;
+    Task: any;
+}
+
+
+@State<OrganaizerStateModel>({
+    name: "Organaizer",
+    defaults: {
+        Day: '',
+        Month: '',
+        Task: ''
+    }
+})
 export class OrganaizerState {
+
 
 }
