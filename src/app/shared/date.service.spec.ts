276a1e8d9ad6f3dfc502e3a376e64deeb82fca37
Кондратьев Павел Дмитриевич <p.kondratiev@scout-gps.ru>
2020-02-28 17:21:30 +0300

    [dev][fix] Добавил testBed в тесты

--- src/app/shared/date.service.spec.ts
+++ src/app/shared/date.service.spec.ts
@@ -1,17 +1,22 @@
+import { TestBed, async } from '@angular/core/testing';
+
 import { DateService } from './date.service';
 
 describe('Service: DateService', () => {
     let service: DateService;
 
-    beforeEach(() => {
-        service = new DateService();
-    });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [DateService]
+        }).compileComponents();
+    }));
 
     afterEach(() => {
         service = null;
     });
 
     it('should create', () => {
+        service = TestBed.get(DateService);
         expect(service).toBeTruthy();
     });
 });
--- src/app/shared/moment.pipe.spec.ts
+++ src/app/shared/moment.pipe.spec.ts
@@ -1,7 +1,15 @@
 import { MomentPipe } from './moment.pipe';
+import { async, TestBed } from '@angular/core/testing';
 
 describe('Pipe: MomentPipe', () => {
     let pipe: MomentPipe;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [MomentPipe]
+        }).compileComponents();
+    }));
+
 
     beforeEach(() => {
         pipe = new MomentPipe();
